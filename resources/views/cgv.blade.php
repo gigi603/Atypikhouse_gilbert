3413f56d25a87fc5318cb53f0bca63fc0e891762
gigi603 <gilbert.trinidad1@gmail.com>
2019-06-02 23:04:31 +0200

    cgv

---
+++ resources/views/cgv.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('title', 'CGV')
+@section('content')
+<div class="container list-category margin-top" role="cgu">
+    <h2 class="h2-title" role="conditions generales de vente">Conditions Générale de vente</h2>
+    <div class="row">
+        <div class="container">
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage1.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage2.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage3.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage4.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage5.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage6.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage7.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage8.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage9.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage10.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage11.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage12.png') }}"/><br/>
+            <img style="width:800px;" src="{{ asset('public/img/cgv/cgvpage13.png') }}"/><br/>
+        </div>
+    </div>
+</div>
+@endsection
