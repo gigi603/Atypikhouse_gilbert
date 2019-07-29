947ccabaa87dea9dfc4d96df4730579392a586a4
gigi603 <gilbert.trinidad1@gmail.com>
2019-07-29 11:16:43 +0200

    fix error request createhousestep2 at 5 files

--- app/Http/Requests/CreateHouseStep2Request.php
+++ app/Http/Requests/CreateHouseStep2Request.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 
-class CreateHouseStep1Request extends FormRequest
+class CreateHouseStep2Request extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
