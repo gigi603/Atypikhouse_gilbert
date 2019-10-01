390b4f2c7f4cc5f25948bb1d017d4f8cb2bac3c6
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-01 23:06:22 +0200

    politique de confidentialité

--- resources/views/politique_de_confidentialite.blade.php
+++ resources/views/politique_de_confidentialite.blade.php
@@ -1,32 +1,6 @@
 @extends('layouts.app')
 @section('title', 'Politique de confidentialité')
 @section('content')
-<div class="container-fluid banner">
-    <div class="intro-body">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="input-group">
-                        <span class="input-group-btn">
-                            <form class="form-horizontal" method="get" action="{{url('search')}}" enctype="multipart/form-data">
-                                <div class="row">
-                                    <div class="col-lg-12 col-md-12 col-sm-12 cadre">
-                                        <h1 class="title title-intro">Trouvez les meilleurs locations atypique, <br />partout en Europe !</h1>
-                                        <div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-3 col-sm-9 col-sm-offset-1">
-                                            <div class="form-group button2">
-                                                @include('search',['url'=>'search','link'=>'search'])
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-                        </span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 <div class="container list-category" role="politique-confidentialite">
     <h2>Politique de confidentialite</h2>
     <div class="row">
