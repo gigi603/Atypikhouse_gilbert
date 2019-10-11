5823b9b752417a2ac6db77df7a5fe26c1759eb80
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-11 14:44:25 +0200

    protection xss

--- vendor/laravel/framework/src/Illuminate/Http/Middleware/FrameGuard.php
+++ vendor/laravel/framework/src/Illuminate/Http/Middleware/FrameGuard.php
@@ -16,7 +16,9 @@ class FrameGuard
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-
+        
+        $response->headers->set('X-XSS-Protection', '1; mode=block');
+        $response->headers->set('Content-Type','text/html; charset=UTF-8');
         $response->headers->set('X-Frame-Options', 'SAMEORIGIN', true);
 
         return $response;
