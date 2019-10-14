8a3633e6dbb96f62ee7d9e06e64d8468ccb62d48
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-14 23:55:32 +0200

    cgu dans la page d'inscription

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -91,6 +91,7 @@
                                 @endif
                             </div>
                         </div>
+                        <p>Le site accessible par l’url www.atypikhouse.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales. En utilisant le site, vous reconnaissez avoir 18 ans ou plus et avoir pris connaissance de ces conditions et les avoir acceptées. Celles-ci pourront êtres modifiées à tout moment et sans préavis par la société Nom de la boite. Eurodev Agency ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.</p>
                         <div class="form-check{{ $errors->has('majeur') ? ' has-error' : '' }} text-center">
                             <input type="checkbox" class="form-check-input" name="majeur" value="true" {{ !old('majeur') ?: 'checked' }}>
                             <label class="form-check-label" for="exampleCheck1">Vous avez 18 ans ou plus</label>
