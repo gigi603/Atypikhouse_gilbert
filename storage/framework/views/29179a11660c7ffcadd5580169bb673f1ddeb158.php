69bfffb9e332cd2e871b540afab03344d58135b6
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-27 01:24:14 +0200

    config gitignore

--- storage/framework/views/29179a11660c7ffcadd5580169bb673f1ddeb158.php
+++ storage/framework/views/29179a11660c7ffcadd5580169bb673f1ddeb158.php
@@ -162,20 +162,25 @@
                 window.cookieconsent.initialise({
                     "palette": {
                         "popup": {
-                            "background": "#efefef",
-                            "text": "#404040"
+                        "background": "#efefef",
+                        "text": "#404040"
                         },
                         "button": {
-                            "background": "#8ec760",
-                            "text": "#ffffff"
+                        "background": "#8ec760",
+                        "text": "#ffffff"
                         }
                     },
+                    "type": "opt-in",
                     "content": {
-                        "message": "AtypikHouse utilisent des coockies pour assurer votre sécuriter et vous offrire une experiences des plus agréable , vous pouvez accéder a notre politique de confidentialité pour avoir un aperçus précis de l'utilisation de ces dernier",
+                        "message": "Le respect de votre vie privée est notre priorité. Nos partenaires et nous-mêmes exploitons différentes technologies, telles que celle des cookies, et traitons vos données à caractère personnel, telles que les adresses IP et les identifiants de cookie, afin de personnaliser les publicités et les contenus en fonction de vos centres d’intérêt, d’évaluer la performance de ces publicités et contenus, et de recueillir des informations sur les publics qui les ont visionnés. Cliquez ci-dessous si vous consentez à l’utilisation de cette technologie et au traitement de vos données à caractère personnel en vue de ces objectifs. Vous pouvez changer d’avis et modifier votre consentement à tout moment en revenant sur ce site. Vous pouvez voir notre politique de confidentialité",
+                        "dismiss": "Got it",
+                        "allow": "Accepter les cookies",
+                        "deny": "Refuser",
                         "link": "ici",
-                        "href": "<?php echo e(route('politique_de_confidentialite')); ?>"
+                        "href": "<?php echo e(route('politique_de_confidentialite')); ?>",
+                        "policy": 'Cookies',
                     }
-                })
+                });
             });
         </script>
         <?php echo $__env->yieldContent('script'); ?>
--- storage/framework/views/3d00e8ae2239eedf4a65e79b85727d38d24da534.php
+++ storage/framework/views/3d00e8ae2239eedf4a65e79b85727d38d24da534.php
@@ -73,5 +73,4 @@
     </div>
 </div>
 <?php $__env->stopSection(); ?>
-
 <?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
