56bab22228c551eae1f26024c55d960d00d70e7d
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-28 12:11:29 +0100

    app/Http/Requests/CreateHouseStep5Request.php

--- app/Http/Requests/CreateHouseStep5Request.php
+++ app/Http/Requests/CreateHouseStep5Request.php
@@ -24,7 +24,7 @@ class CreateHouseStep5Request extends FormRequest
     public function rules()
     {
         return [
-            'photo' => 'image|mimes:jpg,png,jpeg|size:500|dimensions:min_width=450,min_height=300,max_width=1280,max_height=1000',
+            'photo' => 'image|mimes:jpg,png,jpeg|dimensions:min_width=450,min_height=300,max_width=1280,max_height=1000',
         ];
     }
 
@@ -38,7 +38,6 @@ class CreateHouseStep5Request extends FormRequest
         return [
             'photo.required' => 'Veuillez mettre une photo de votre hebergement',
             'photo.image' => 'Veuillez mettre une image au format jpg,png ou jpeg',
-            'photo.size' => "L'image doit faire minimum 500ko",
             'photo.dimensions' => 'Votre doit faire minimum 450px de largeur et 300 de hauteur'
         ];
     }
