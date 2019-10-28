850f264545d31c68a257d383d112eccc95d4f091
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-28 20:45:23 +0100

    layout admin-login

---
+++ resources/views/layouts/admin-login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+   <!-- CSRF Token -->
+   <meta name="csrf-token" content="{{ csrf_token() }}">
+
+   {{-- Title --}}
+  <title>Atypikhouse - Connexion admin</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="{{ asset('css/fontawesome-free/all.min.css') }}" rel="stylesheet" type="text/css">
+  <!-- Custom styles for this template-->
+  <link href="{{ asset('css/sb-admin.css') }}" rel="stylesheet">
+
+</head>
+
+<body class="bg-dark">
+    @yield('content')
+</body>
+</html>
