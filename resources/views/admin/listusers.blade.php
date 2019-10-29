f8d824783c7ab819b264c9ab0c2f9f93d7d5fc54
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-29 12:13:27 +0100

    new table dashboard admin layout

--- resources/views/admin/listusers.blade.php
+++ resources/views/admin/listusers.blade.php
@@ -1,36 +1,5 @@
 @extends('layouts.admin')
 @section('content')
-<div id="utilisateur">
-    <h2>Utilisateurs : </h2>
-    @foreach($users as $user)
-    <div class="table-responsive">
-        <table class="table">
-            <tbody>
-                <tr>
-                    <td><a href="{{action('AdminController@profilUser', $user['id'])}}">{{$user->nom}} {{$user->prenom}}</a></td>
-                    <td>{{$user->email}}</td>
-                    <td>Compte activé : {{$user->statut}}</td>
-                    <td>
-                        <a href="{{action('AdminController@profilUser', $user['id'])}}" class="btn btn-success">Voir ses annonces/ autres informations</a>
-                    </td>
-                    @if($user->statut ==  1)
-                        <td>
-                            <a href="{{ route('admin.disable_user', $user->id) }}" class="delete-user btn btn-danger">Désactiver le compte</a>
-                        </td>
-                    @else
-                        <td>
-                            <a href="{{ route('admin.activate_user', $user->id) }}" class="btn btn-success">Activer le compte</a>
-                        </td>
-                    @endif
-                </tr>
-            </tbody>
-        </table>
-    </div>
-    @endforeach
-</div>
-
-
-
 <div class="card mb-3">
     <div class="card-header">
       <i class="fas fa-table"></i>
