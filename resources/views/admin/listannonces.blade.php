5aff8c9a2e6270943924a76b2d30f88a01adce47
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-30 10:24:29 +0100

    showhouse

--- resources/views/admin/listannonces.blade.php
+++ resources/views/admin/listannonces.blade.php
@@ -1,55 +1,52 @@
 @extends('layouts.admin')
 @section('content')
-<div class="admin-user-profil">
-    <div class="col-offset-md-2 col-md-10 right">
-        @foreach($users as $user)
-            <div class="panel panel-default">
-                <div class="panel-heading text-center">Annonces de {{$user->nom}} {{$user->prenom}}</div>
-                @if (\Session::has('success'))
-                    <div class="alert alert-success">
-                        <ul>
-                            <p>{!! \Session::get('success') !!}</p>
-                        </ul>
-                    </div>
-                @endif
-                @foreach ($houses as $house)           
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-4 col-md-4 mb-4 thumbnail card-admin">
-                                <div class="card h-100">
-                                    <img class="img-responsive img_house" src="{{ asset('img/houses/'.$house->photo) }}">
-                                    <div>
-                                        <h4 class="title card-title">
-                                            <a href="{{route('admin.showannonces', $house['id']) }}">{{$house->title}}</a>
-                                        </h4>
-                                        <p class="price">{{$house->price}}€ par nuit</p>
-                                        <div class="card-infos">
-                                            <p>Type de bien : {{$house->category->category}}</p>     
-                                            <p><?php echo(substr($house->description, 0, 40));?></p>   
-                                            <p>Annulation gratuite !</p>
-                                            <p> Location : {{$house->adresse}}</p>
-                                        @if($house->statut == "En attente de validation")
-                                            <p>Statut: <span style="color:red;"><?php echo($house->statut);?></span></p>
-                                        @else
-                                            <p>Statut: <span style="color:green;"><?php echo($house->statut);?></span></p>
-                                        @endif
-                                        <div class="row">
-                                            <div class="form-group">
-                                                <div class="col-md-12 text-center">
-                                                    <a href="{{route('admin.editHouse', $house['id']) }}" class="btn btn-primary btn-color">Modifier</a>
-                                                    <a href="{{route('admin.disableHouse', $house['id']) }}" class="btn btn-danger delete-annonce">Supprimer</a>
-                                                    <a href="{{route('admin.valideHouse', $house['id']) }}" class="btn btn-primary btn-color">Valider l'annonce</a>
-                                                    <a href="{{route('admin.refuseHouse', $house['id']) }}" class="btn btn-danger delete-annonce">Refuser l'annonce</a>
-                                                </div>
-                                            </div>                      
-                                        </div>
-                                    </div>
-                                </div> 
-                            </div>
-                        </div>
-                    @endforeach
+    @foreach($users as $user)
+        <div class="panel panel-default">
+            <div class="panel-heading text-center">Annonces de {{$user->nom}} {{$user->prenom}}</div>
+            @if (\Session::has('success'))
+                <div class="alert alert-success">
+                    <ul>
+                        <p>{!! \Session::get('success') !!}</p>
+                    </ul>
                 </div>
+            @endif
+            @foreach ($houses as $house)           
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-4 mb-4 thumbnail card-admin">
+                            <div class="card h-100">
+                                <img class="img-responsive img_house" src="{{ asset('img/houses/'.$house->photo) }}">
+                                <div>
+                                    <h4 class="title card-title">
+                                        <a href="{{route('admin.showannonces', $house['id']) }}">{{$house->title}}</a>
+                                    </h4>
+                                    <p class="price">{{$house->price}}€ par nuit</p>
+                                    <div class="card-infos">
+                                        <p>Type de bien : {{$house->category->category}}</p>     
+                                        <p><?php echo(substr($house->description, 0, 40));?></p>   
+                                        <p>Annulation gratuite !</p>
+                                        <p> Location : {{$house->adresse}}</p>
+                                    @if($house->statut == "En attente de validation")
+                                        <p>Statut: <span style="color:red;"><?php echo($house->statut);?></span></p>
+                                    @else
+                                        <p>Statut: <span style="color:green;"><?php echo($house->statut);?></span></p>
+                                    @endif
+                                    <div class="row">
+                                        <div class="form-group">
+                                            <div class="col-md-12 text-center">
+                                                <a href="{{route('admin.editHouse', $house['id']) }}" class="btn btn-primary btn-color">Modifier</a>
+                                                <a href="{{route('admin.disableHouse', $house['id']) }}" class="btn btn-danger delete-annonce">Supprimer</a>
+                                                <a href="{{route('admin.valideHouse', $house['id']) }}" class="btn btn-primary btn-color">Valider l'annonce</a>
+                                                <a href="{{route('admin.refuseHouse', $house['id']) }}" class="btn btn-danger delete-annonce">Refuser l'annonce</a>
+                                            </div>
+                                        </div>                      
+                                    </div>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
-    </div>
-</div>
+        </div>
+    @endforeach
+@endsection
