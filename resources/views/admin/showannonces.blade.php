32e1105faa0dfc46c264913df7da96f0399f87e5
gigi603 <gilbert.trinidad1@gmail.com>
2019-10-31 10:40:32 +0100

    css pour showannonce

--- resources/views/admin/showannonces.blade.php
+++ resources/views/admin/showannonces.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.admin')
 @section('title', "Détails de l'annonce")
-<link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-<link href="../vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
-<link href="../css/sb-admin.css" rel="stylesheet">
-<link href="../css/custom-admin.css" rel="stylesheet">
 @section('content')
 <div class="admin-user-profil">
     <div class="container list-category">
@@ -101,22 +97,6 @@
 </div> 
 @endsection
 @section('script')
-    <!-- Bootstrap core JavaScript-->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Page level plugin JavaScript-->
-    <script src="../vendor/chart.js/Chart.min.js"></script>
-    <script src="../vendor/datatables/jquery.dataTables.js"></script>
-    <script src="../vendor/datatables/dataTables.bootstrap4.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="../js/sb-admin.min.js"></script>
-
-    <!-- Demo scripts for this page-->
-    <script src="../js/demo/datatables-demo.js"></script>
-    <script src="../js/demo/chart-area-demo.js"></script>
+    <script src="{{ asset('js/jquery.js') }}"></script>
+    <script src="{{ asset('js/jquery-ui.min.js') }}"></script>
 @endsection
