a9bc07a7c8c0b451d6295673a90c95560946cec7
gigi603 <gilbert.trinidad1@gmail.com>
2019-11-11 14:09:38 +0100

    gerer le formulaire de contact côté back

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -9,11 +9,12 @@ use App\Admin;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreatePostRequest;
 use App\Notifications\ReplyToThread;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
     public function index()  
-    { 
+    {
         return view('posts.index'); 
     } 
  
@@ -22,19 +23,28 @@ class PostsController extends Controller
         $this->validate($request, [ 
             'name' => 'required|max:100|regex:/^[a-zA-Z\s\-]+$/u', 
             'email' => 'required|max:50|email', 
-            'content' => 'required|max:3000|regex:/^[0-9\pL\s\'\-\()\.\,\@\?\!\;\"\:]+$/u' 
+            'content' => 'required|max:3000|min:30|regex:/^[0-9\pL\s\'\-\()\.\,\@\?\!\;\"\:]+$/u' 
         ]); 
-        $post = new post;
-        $post->name = $request->name;
-        $post->email = $request->email;
-        $post->content = $request->content;
-        $post->save();
-        
-        $admins = Admin::all();
-        foreach ($admins as $admin) {
-            $admin->notify(new ReplyToThread($post));
-        }
 
-        return back()->with('success', 'Votre message a bien été envoyé, nos administrateurs reviendront vers vous !');
+        //$user = $request->user();
+        var_dump(Auth::user()->nom);
+
+        if($request->name != Auth::user()->nom.' '.Auth::user()->prenom || $request->email != Auth::user()->email){
+            return back()->with('danger', "Votre message n'a pas été envoyé");
+        } else {
+
+            $post = new post;
+            $post->name = $request->name;
+            $post->email = $request->email;
+            $post->content = $request->content;
+            $post->save();
+        
+            $admins = Admin::all();
+            foreach ($admins as $admin) {
+                $admin->notify(new ReplyToThread($post));
+            }
+
+            return back()->with('success', 'Votre message a bien été envoyé, nos administrateurs reviendront vers vous !');
+        }
     }
 }
--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -4,3 +4,10 @@
     <p>{{ $message }}</p>
 </div>
 @endif
+
+@if ($message = Session::get('danger'))
+<div class="alert alert-danger alert-block">
+    <button type="button" class="close" data-dismiss="alert">x</button>
+    <p>{{ $message }}</p>
+</div>
+@endif
