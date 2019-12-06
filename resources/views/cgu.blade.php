79534c80541a32b0ceb7a541f597e658659e50ec
gigi603 <gilbert.trinidad1@gmail.com>
2019-12-06 11:03:03 +0100

    modifier le cgu

--- resources/views/cgu.blade.php
+++ resources/views/cgu.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('title', 'CGU')
 @section('content')
-<div class="container list-category" role="cgu">
-    <h2>Conditions Générale d'utilisation</h2>
+<div class="container" role="cgu">
+    <h2 class="cgu-title">Conditions Générale d'utilisation</h2>
     <div class="row">
-        <div class="container">
-            <p>Le site accessible par l’url www.atypikhouse.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales. En utilisant le site, vous reconnaissez avoir pris connaissance de ces conditions et les avoir acceptées. Celles-ci pourront êtres modifiées à tout moment et sans préavis par la société Nom de la boite. Eurodev Agency ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.
-</p>
+        <div class="cgu-content">
+            <p>Le site accessible par l’url www.atypikhouse.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales. En utilisant le site, vous reconnaissez avoir 18 ans ou plus et avoir pris connaissance de ces conditions et les avoir acceptées. Celles-ci pourront êtres modifiées à tout moment et sans préavis par la société Nom de la boite. Eurodev Agency ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.
+            </p>
         </div>
     </div>
 </div>
