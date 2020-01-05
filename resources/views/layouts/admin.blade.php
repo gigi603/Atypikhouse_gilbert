4a279ce25f2cc05f8bb17b1b879afe9520418b0a
gigi603 <gilbert.trinidad1@gmail.com>
2020-01-06 00:34:31 +0100

    logout et liste des messages dans le menu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,39 +48,7 @@
 
     <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
-      <li class="nav-item dropdown no-arrow mx-1">
-        <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-bell fa-fw"></i>
-          <span class="badge badge-danger">9+</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alertsDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown no-arrow mx-1">
-        <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-envelope fa-fw"></i>
-          <span class="badge badge-danger">7</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="messagesDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown no-arrow">
-        <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-user-circle fa-fw"></i>
-        </a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="{{ route('admin.logout') }}" data-toggle="modal" data-target="#logoutModal">Se déconnecter</a>
-        </div>
-      </li>
+      <li><a class="dropdown-item" href="{{ route('admin.logout') }}">Se déconnecter</a></li>
     </ul>
 
   </nav>
@@ -123,6 +91,12 @@
         <a class="nav-link" href="{{ route('admin.allreservationscancel') }}">
           <i class="fas fa-fw fa-folder"></i>
           <span>Réservations annulées</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('admin.messages') }}">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Messages des clients</span>
         </a>
       </li>
     </ul>
