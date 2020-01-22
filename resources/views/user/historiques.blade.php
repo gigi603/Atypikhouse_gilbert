f9c79373ddbd69f5dd4c3800195a0cc703abddd4
gigi603 <gilbert.trinidad1@gmail.com>
2020-01-22 11:16:38 +0100

    fix bug in historiques page

--- resources/views/user/historiques.blade.php
+++ resources/views/user/historiques.blade.php
@@ -37,7 +37,7 @@
                         @else
                             <p>Statut: <span style="color:red;">Annulée</span></p>
                         @endif
-                        <h4 class="text-center"><a href="{{route('user.showhistoriques', $reservation['id']) }}" class="btn btn-primary btn-color">Voir</a></h4>
+                        <h4 class="text-center"><a href="{{route('user.showhistoriques', $historique['id']) }}" class="btn btn-primary btn-color">Voir</a></h4>
                     </div>
                 </div> 
             </div>
