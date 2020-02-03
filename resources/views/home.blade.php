b2dd443186cc057455818dbbf0be1f0034367db4
gigi603 <gilbert.trinidad1@gmail.com>
2020-02-03 22:02:30 +0100

    put image of houses in home page in the same size

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,7 +47,7 @@
                 @if($house->statut == "Validé")
                     <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">         
                         <div class="card-houses h-100">       
-                            <a href="{{action('UsersController@showHouse', $house['id'])}}"><img class="img-responsive" src="{{ asset('img/houses/'.$house->photo) }}" alt="Hébergement insolite - {{$house->title}}"></a>
+                            <a href="{{action('UsersController@showHouse', $house['id'])}}"><img class="img-houses-list" src="{{ asset('img/houses/'.$house->photo) }}" alt="Hébergement insolite - {{$house->title}}"></a>
                             <div class="card-block">
                                 <div class="card-body">
                                     <h3 class="card-title"><a href="{{action('UsersController@showHouse', $house->id)}}"> <?php echo(substr($house->title, 0, 30));?> </a></h3>
