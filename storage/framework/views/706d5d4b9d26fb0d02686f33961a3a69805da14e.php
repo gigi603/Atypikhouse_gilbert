26477c6ac542656a25379b3732d09bf11aa58e58
gigi603 <gilbert.trinidad1@gmail.com>
2020-07-20 16:12:30 +0200

    finish

---
+++ storage/framework/views/706d5d4b9d26fb0d02686f33961a3a69805da14e.php
@@ -0,0 +1,16 @@
+<?php $__env->startSection('title', "Conditions générales de d'utilisation"); ?>
+<?php $__env->startSection('meta_description', "Nous vous mettons à disposition nos conditions générale d'utilisation"); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="container" role="condition générale d'utilisation du site atypikhouse">
+    <h1 class="cgu-title">Conditions Générale d'utilisation</h1>
+    <div class="row">
+        <div class="cgu-content">
+            <p>Le site accessible par l’url www.atypikhouse.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales. En utilisant le site, vous reconnaissez avoir 18 ans ou plus et avoir pris connaissance de ces conditions et les avoir acceptées. Celles-ci pourront êtres modifiées à tout moment et sans préavis par la société Nom de la boite. Eurodev Agency ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.
+            </p>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
