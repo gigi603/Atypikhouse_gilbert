f2413decc41fd1d338b3c380ebfa7b90f2e8b0a8
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-16 14:16:24 +0200

    mise à jour des informations sur la page cgu

--- resources/views/cgu.blade.php
+++ resources/views/cgu.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
             <div class="container">
                 <div class="block">
-                    <p>Le site accessible par l’url www.atypikhouse.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales.  En utilisant le site, vous reconnaissez avoir 18 ans ou plus et vous autorisez atypikhouse à vous contacter de façon personnalisée à propos de ses services pour assurer votre sécurité et améliorer les services proposer par atypikhouse. Vos données personnelles ne seront jamais communiquées à des tiers. Après avoir pris connaissance de ces conditions et les avoir acceptées, celles-ci pourront êtres modifiées à tout moment et sans préavis par la société. Eurodev Agency ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.</p>
+                    <p>Le site accessible par l’url www.atypikhouse.fr est exploité dans le respect de la législation française. L'utilisation de ce site est régie par les présentes conditions générales.  En utilisant le site, vous reconnaissez avoir 18 ans ou plus et vous autorisez atypikhouse à vous contacter de façon personnalisée à propos de ses services pour assurer votre sécurité et améliorer les services proposés par atypikhouse. Vos données personnelles ne seront jamais communiquées à des tiers. Après avoir pris connaissance de ces conditions et les avoir acceptées, celles-ci pourront êtres modifiées à tout moment et sans préavis par la société. Eurodev Agency ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation du service.</p>
                 </div>
             </div>
         </div>
