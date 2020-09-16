92415f45a7c4452b757a4bca156bafb8d01b9851
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-16 14:47:25 +0200

    change texte of newsletter

--- resources/views/email/newsletters/index.blade.php
+++ resources/views/email/newsletters/index.blade.php
@@ -1,4 +1,4 @@
  <h1><?php echo $newsletterTitle; ?> </h1>
-<p>Pour une réduction de <?php echo $newsletterReduction;?> ce qui fait un montant de <?php echo $newsletterPrice;?> !</p>
+<p>Avec une réduction de <?php echo $newsletterReduction;?>% sur l'annonce "Cabane de trappeur" le montant d'une nuit passe de 99 euros à 49,50 euros !</p>
 <p>Cette annonce se trouve à l'adresse: <?php echo $newsletterAdresse;?>  <br/> dont la description est: <?php echo $newsletterDescription;?>.
 Notre équipe vous remercie de votre fidélité</p>
