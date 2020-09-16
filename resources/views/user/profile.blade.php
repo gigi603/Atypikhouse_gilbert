1ab07ccf7752b15ee0600cffb98193c673bfe695
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-16 15:08:01 +0200

    profile refonte front

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,36 +2,38 @@
 @section('title', 'Utilisateur')
 @section('footer', 'footer_absolute')
 @section('content')
-
-<div class="container container-profile">
+<div class="container margin-top block-size" role="condition générale d'utilisation du site atypikhouse">
+    <h1 class="h1-title">Mon profil</h1>
     <div class="row">
-        <div class="col-xs-12 col-sm-6 col-md-12">
-            <div class="well well-sm">
-                <div class="row">
-                    <div class="col-sm-6 col-md-4">
-                    </div>
-                    <div class="col-sm-6 col-md-8">
-                        <form class="form-horizontal" method="POST" action="{{action('UsersController@edit', Auth::user()->id)}}" enctype="multipart/form-data">                      
-                            {{ csrf_field() }}
-                            <h4>
-                            {{Auth::user()->prenom}} {{Auth::user()->nom}}</h4>
-                            <p>
-                                <i class="glyphicon glyphicon-envelope"></i>{{Auth::user()->email}}
-                            </p>
-                            <br>
-                            <p>
-                            <div class="form-check{{ $errors->has('newsletter') ? ' has-error' : '' }}">
-                                <input type="checkbox" class="form-check-input" name="newsletter" value="1" {{(Auth::user()->newsletter == 1 ? 'checked' : '')}}>
-                                <label class="form-check-label" for="exampleCheck1">Recevoir les newsletters</label>
-                                @if ($errors->has('newsletter'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('newsletter') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                            </p>
-                            <button class="btn btn-primary btn-color">Modifier</button>
-                        </form>
+        <div class="container">
+            <div class="block">
+                <div class="well well-sm">
+                    <div class="row">
+                        <div class="col-sm-6 col-md-4">
+                        </div>
+                        <div class="col-sm-6 col-md-8">
+                            <form class="form-horizontal" method="POST" action="{{action('UsersController@edit', Auth::user()->id)}}" enctype="multipart/form-data">                      
+                                {{ csrf_field() }}
+                                <h4>
+                                {{Auth::user()->prenom}} {{Auth::user()->nom}}</h4>
+                                <p>
+                                    <i class="glyphicon glyphicon-envelope"></i>{{Auth::user()->email}}
+                                </p>
+                                <br>
+                                <p>
+                                <div class="form-check{{ $errors->has('newsletter') ? ' has-error' : '' }}">
+                                    <input type="checkbox" class="form-check-input" name="newsletter" value="1" {{(Auth::user()->newsletter == 1 ? 'checked' : '')}}>
+                                    <label class="form-check-label" for="exampleCheck1">Recevoir les newsletters</label>
+                                    @if ($errors->has('newsletter'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('newsletter') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                </p>
+                                <button class="btn btn-primary btn-color">Modifier</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
