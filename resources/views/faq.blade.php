1c2b107097742843ff9fcc7616736ad365385260
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-16 12:00:33 +0200

    upgrade faq with footer at the bottom

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
 @section('title', 'Faq')
 @section('meta_description', "Nous vous mettons à disposition une faq ayant pour but de répondre à vos questions, si la faq ne répond pas à vos attentes, vous pouvez nous contacter via notre formulaire de contact")
-@section('footer', 'footer_absolute')
-
 @section('content')
-<div class="container list-category faq" role="faq">
-    <h1 id="faq">FAQ : </h1>
+<div class="container margin-top block-size" role="faq">
+    <h1 id="faq">FAQ </h1>
     <div class="row">
         <div class="container">
             <div id="faq">
-                <h2>Mon paiement est-it sécurisée ?</h2>
-                <p>Nous disposons d’un système de crytpage SSL pour protéger vos données personnelles ainsi que les moyens de paiement utilisés. Nous utilisons le système de paiement sécurisé de Stripe</p>
                 <h2>SERVICE CLIENT 24/7</h2>
-                <p>Notre équipe est à votre disposition pour toute question sur nos articles, votre commande ou autre question d'ordre générale</p>
+                <p>Notre équipe est à votre disposition pour toute question sur nos annonces, votre reservation et/ou votre annonce ou toute autre question d'ordre générale</p>
                 <h2>Mon paiement est-it sécurisée ?</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id deserunt pariatur praesentium, dolor obcaecati quia qui iste nam? Est nesciunt blanditiis magni ab omnis architecto deserunt maiores, sapiente quaerat quibusdam.</p>
+                <p>Avec le logiciel et API de Stripe qui est utilisé par un bon nombres d'entreprises tels que deliveroo, booking.com ou bien mano mano, nous garantissons un paiement sécurisée afin de combattre la fraude de paiement en ligne.</p>
             </div>
         </div>
     </div>
 </div>
+@endsection
+@section('footer', 'footer_absolute')
+@section('script')
 @endsection
