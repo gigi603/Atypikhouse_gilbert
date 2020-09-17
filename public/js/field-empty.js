534da79c424ad92412f4806ae9bd4b634415f54c
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-17 11:52:46 +0200

    message select validation

--- public/js/field-empty.js
+++ public/js/field-empty.js
@@ -75,3 +75,16 @@ $('.form-horizontal hidden').on('change invalid', function() {
     textfield.setCustomValidity('Veuillez remplir ce champ');  
   }
 });
+
+$('.form-horizontal select').on('change invalid', function() {
+  var textfield = $(this).get(0);
+  
+  // 'setCustomValidity not only sets the message, but also marks
+  // the field as invalid. In order to see whether the field really is
+  // invalid, we have to remove the message first
+  textfield.setCustomValidity('');
+  
+  if (!textfield.validity.valid) {
+    textfield.setCustomValidity('Veuillez selectionner une valeur');  
+  }
+});
