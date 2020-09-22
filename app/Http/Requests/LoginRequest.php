12ed90d129f2d8be30c046671701a00651546f4f
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-22 14:10:45 +0200

    min max password

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -24,8 +24,8 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            "email" => "required|email",
-            "password" => "required|regex:/^[0-9\pL\s\d\'\’\-\(\)\.\,\@\?\!\;\^\"\:]+$/u"
+            "email" => "required|email|max:30",
+            "password" => "required|regex:/^[0-9\pL\s\d\'\’\-\(\)\.\,\@\?\!\;\^\"\:]+$/u|min:8|max:30"
         ];
     }
 
@@ -38,8 +38,11 @@ class LoginRequest extends FormRequest
     {
         return [
             'email.required' => 'Veuillez saisir une adresse email',
+            'email.max' => 'Votre adresse peut contenir maximum 30 caractères',
             'email.email' => 'Veuillez saisir une adresse email valide',
             'password.required' => "Un mot de passe est requis",
+            'password.min' => "Votre mot de passe doit contenir minimum 8 caractères",
+            'password.max' => "Votre mot de passe doit contenir maximum 30 caractères",
             'password.regex' => "Les caractères spéciaux autorisés sont : les ponctuations, slash, arobathes, tirets, apostrophes, parentheses et les guillemets"
         ];
     }
