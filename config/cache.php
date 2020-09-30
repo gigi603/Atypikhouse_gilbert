53f321d1abae82987b6d4677b00d46e2e0fb8527
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-30 15:02:29 +0200

    replace 127.0.0.1 in cache.php

--- config/cache.php
+++ config/cache.php
@@ -61,7 +61,7 @@ return [
             ],
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'host' => env('MEMCACHED_HOST', 'atypikhouse-project.ovh'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
