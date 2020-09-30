790efee7d6de6e8edbd0b4eea23bf3b66e2a456e
gigi603 <gilbert.trinidad1@gmail.com>
2020-09-30 14:46:30 +0200

    apostophe for lastname and firstname

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -104,8 +104,8 @@ class RegisterController extends Controller
            $input = $request->all();
 
             $validator = $this->validate($request, [
-                'nom' => 'required|max:30|regex:/^[\pL\s\-]+$/u',
-                'prenom' => 'required|min:1|max:30|regex:/^[\pL\s\-]+$/u',
+                'nom' => 'required|max:30|regex:/^[\pL\s\-\']+$/u',
+                'prenom' => 'required|min:1|max:30|regex:/^[\pL\s\-\']+$/u',
                 'email' => 'required|unique:users|max:30|email',
                 'email_confirmation' => 'required|same:email|max:30',
                 'password' => 'required|min:8|max:30|regex:/^[0-9\pL\s\d\'\’\-\(\)\.\,\?\!\;\^\"\:]+$/u',
