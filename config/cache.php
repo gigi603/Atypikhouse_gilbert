297b47135dee7d1cfd919137fbd5807532f46913
gigi603 <gilbert.trinidad1@gmail.com>
2020-10-02 02:44:10 +0200

    cache prod

--- config/cache.php
+++ config/cache.php
@@ -61,7 +61,7 @@ return [
             ],
             'servers' => [
                 [
-                    'host' => env('MEMCACHED_HOST', 'atypikhouse-project.ovh'),
+                    'host' => env('MEMCACHED_HOST', 'atypikhouse-projet.ovh'),
                     'port' => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
