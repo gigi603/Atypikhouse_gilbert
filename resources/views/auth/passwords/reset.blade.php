1d1bab8b81e6ab074e35d890571fa57d10222426
gigi603 <gilbert.trinidad1@gmail.com>
2020-10-06 04:47:23 +0200

    h1reinitialisation mot de passe

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Réinitialiser mon mot de passe</div>
+                <div class="panel-heading"><h1 style="font-size:30px;">Réinitialiser mon mot de passe</h1></div>
 
                 <div class="panel-body">
                     <form class="form-horizontal" method="POST" action="{{ route('password.request') }}">
